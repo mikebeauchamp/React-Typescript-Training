fedf10165f700a1ad58cd958b9485785638d62f0
Michael Beauchamp <beauchampmichaele@gmail.com>
2026-02-17 16:09:45 -0500

    refactor: update user fetching logic to use async/await and improve error handling

--- connecting-to-api-exercise/src/App.tsx
+++ connecting-to-api-exercise/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { useEffect, useState } from 'react'
 
 interface User {
@@ -14,14 +14,27 @@ const App = () => {
     const [error, setError] = useState('')
 
     useEffect(() => {
-        axios
-            .get<User[]>('https://jsonfakery.com/usersss/random/3')
-            .then(response => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get<User[]>(
+                    'https://jsonfakery.com/users/random/3'
+                )
                 setUsers(response.data)
-            })
-            .catch(error => {
-                setError(error.message)
-            })
+            } catch (error) {
+                setError((error as AxiosError).message)
+            }
+        }
+
+        fetchUsers()
+
+        // axios
+        //     .get<User[]>('https://jsonfakery.com/usersss/random/3')
+        //     .then(response => {
+        //         setUsers(response.data)
+        //     })
+        //     .catch(error => {
+        //         setError(error.message)
+        //     })
     }, [])
 
     return (
