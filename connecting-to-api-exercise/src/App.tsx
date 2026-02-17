6dbdb723f76b3cff90bf7fa6d17d17c92ab7e587
Michael Beauchamp <beauchampmichaele@gmail.com>
2026-02-17 16:59:07 -0500

    feat: enhance user fetching with loading state and error handling; add user creation and deletion functionality

--- connecting-to-api-exercise/src/App.tsx
+++ connecting-to-api-exercise/src/App.tsx
@@ -1,8 +1,8 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, CanceledError } from 'axios'
 import { useEffect, useState } from 'react'
 
 interface User {
-    id: number
+    id: string
     first_name: string
     middle_name: string
     last_name: string
@@ -12,20 +12,31 @@ interface User {
 const App = () => {
     const [users, setUsers] = useState<User[]>([])
     const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        const controller = new AbortController()
         const fetchUsers = async () => {
             try {
+                setLoading(true)
                 const response = await axios.get<User[]>(
-                    'https://jsonfakery.com/users/random/3'
+                    'https://jsonfakery.com/users/random/3',
+                    { signal: controller.signal }
                 )
                 setUsers(response.data)
+                setLoading(false)
             } catch (error) {
+                if (error instanceof CanceledError) return
                 setError((error as AxiosError).message)
+                setLoading(false)
             }
         }
 
         fetchUsers()
+
+        return () => {
+            controller.abort()
+        }
 
         // axios
         //     .get<User[]>('https://jsonfakery.com/usersss/random/3')
@@ -37,14 +48,60 @@ const App = () => {
         //     })
     }, [])
 
+    //Delete user from list
+    const deleteUser = (user: User) => {
+        setUsers(users.filter(u => u.id !== user.id))
+        axios.delete(`https://jsonfakery.com/users/${user.id}`)
+    }
+
+    const addUser = async () => {
+        const newUser: User = {
+            id: '',
+            first_name: 'New',
+            middle_name: 'User',
+            last_name: 'Added',
+            email: 'new.user@example.com'
+        }
+        setUsers([newUser, ...users])
+
+        try {
+            const response = await axios.post(
+                'https://jsonfakery.com/users',
+                newUser
+            )
+            const createdUser = response.data
+            setUsers(prevUsers =>
+                prevUsers.map(user => (user === newUser ? createdUser : user))
+            )
+        } catch (error) {
+            setError((error as AxiosError).message)
+            setUsers(prevUsers => prevUsers.filter(user => user !== newUser))
+        }
+    }
+
     return (
         <>
             {error && <p className="text-red-500">{error}</p>}
-            <ul>
+            {loading && <p>Loading...</p>}
+            <ul className="w-1/2 divide-y divide-gray-300">
+                <button
+                    className="bg-blue-500 text-white px-4 py-1 rounded-lg hover:bg-blue-700 mb-2"
+                    onClick={addUser}
+                >
+                    Create User{' '}
+                </button>
                 {users.map(user => (
-                    <li key={user.id}>
-                        {user.first_name} {user.middle_name} {user.last_name}{' '}
-                        {user.email}
+                    <li
+                        key={user.id}
+                        className="flex justify-between leading-loose py-1"
+                    >
+                        {user.first_name} {user.middle_name} {user.last_name}
+                        <button
+                            className="bg-red-500 text-white px-4 py-1 rounded-lg hover:bg-red-700"
+                            onClick={() => deleteUser(user)}
+                        >
+                            Delete
+                        </button>
                     </li>
                 ))}
             </ul>
